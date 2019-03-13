0661c7925d376c1b6c2a9f145ebf2ec9bb1ffa70
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-03-12 20:29:03 -0400

    update readme

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@
 Route::view('/', 'welcome');
 Route::get('/books', 'BookController@index');
 Route::get('/books/{title}', 'BookController@show');
+
+Route::get("/example", function(){
+    return view('abc');
+});
