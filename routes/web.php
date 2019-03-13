b8d494857fcfc4528bfd33a94d71ae78a9dde2ce
Jeremy C. Southgate <jeremy@jeremysouthgate.com>
2019-03-12 20:14:08 -0400

    add basic page routing

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+################################################################################
+#   Web Routes
+################################################################################
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/a/b/c', function(){
-    return "abc";
-});
-
-Route::get('/example', function(){
-    return view("abc");
-});
+Route::view('/', 'welcome');
+Route::get('/books', 'BookController@index');
+Route::get('/books/{title}', 'BookController@show');
